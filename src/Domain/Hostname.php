f7c9c7e7de373c078b4b91d4074877dd61f9bd20
agares <me@agares.info>
2016-07-21 23:56:14 +0200

    Cleanup Hostname VO

--- src/Domain/Hostname.php
+++ src/Domain/Hostname.php
@@ -4,6 +4,9 @@ namespace Agares\AxeOrchestra\Domain;
 
 class Hostname
 {
+	const LABEL_REGEXP = '/^[a-z0-9][0-9a-z\\-]*$/i';
+	const LABEL_SEPARATOR = '.';
+
 	public static function fromString(string $name)
 	{
 		static::validate($name);
@@ -12,7 +15,7 @@ class Hostname
 	}
 
 	private static function validate(string $name) {
-		$labels = explode('.', $name);
+		$labels = explode(self::LABEL_SEPARATOR, $name);
 
 		foreach($labels as $label)
 			static::validateLabel($label);
@@ -20,7 +23,7 @@ class Hostname
 
 	private static function validateLabel(string $label)
 	{
-		if(!preg_match('/^[a-z0-9][0-9a-z\\-]*$/i', $label))
+		if(!preg_match(self::LABEL_REGEXP, $label))
 			throw InvalidHostnameException::label($label);
 	}
 }
--- src/Domain/InvalidHostnameException.php
+++ src/Domain/InvalidHostnameException.php
@@ -4,10 +4,6 @@ namespace Agares\AxeOrchestra\Domain;
 
 class InvalidHostnameException extends \Exception
 {
-	public static function hostname($hostname) {
-		return new static(sprintf('The hostname "%s" is invalid.', $hostname));
-	}
-
 	public static function label($label) {
 		return new static(sprintf('The label "%s" is invalid', $label));
 	}
